15b535eb93983c4dbf55cbd3ac48982269a2b5ad
YASSER-TAIBI <YASSER-TAIBI>
2022-03-11 10:29:47 +0100

    page activite calendars par jour

--- src/app/activite/activite.component.ts
+++ src/app/activite/activite.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  ScheduleComponent, EventSettingsModel, View, EventRenderedArgs, DayService,
+  WeekService, WorkWeekService, MonthService, ResizeService, DragAndDropService
+} from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-activite',
+    //template: '<ejs-schedule #scheduleObj width="100%" [selectedDate]="setDate" [currentView]="setView"></ejs-schedule>',
   templateUrl: './activite.component.html',
-  styleUrls: ['./activite.component.css']
+  styleUrls: ['./activite.component.css'],
+  providers: [DayService, WeekService, WorkWeekService,
+     MonthService, ResizeService, DragAndDropService]
 })
 export class ActiviteComponent implements OnInit {
 
+  titre= 'activite-app';
+  public setView: View ="Day";
+  public setDate: Date = new Date(Date.now());
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/activite/activite.module.ts
+++ src/app/activite/activite.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ActiviteRoutingModule } from './activite-routing.module';
 import { ActiviteComponent } from './activite.component';
-
+import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService } from '@syncfusion/ej2-angular-schedule';
 
 @NgModule({
   declarations: [ActiviteComponent],
   imports: [
     CommonModule,
-    ActiviteRoutingModule
-  ]
+    ActiviteRoutingModule,
+    ScheduleModule, RecurrenceEditorModule
+  ],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService],
+  bootstrap: [ActiviteComponent]
 })
 export class ActiviteModule { }
