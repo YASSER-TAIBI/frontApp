874963a7a526cca202473064c34ad2252daf6464
YASSER-TAIBI <YASSER-TAIBI>
2022-03-13 01:01:22 +0100

    activite add date

--- src/app/activite/activite.component.ts
+++ src/app/activite/activite.component.ts
@@ -18,9 +18,29 @@ export class ActiviteComponent implements OnInit {
   public setView: View ="Day";
   public setDate: Date = new Date(Date.now());
   
+  
+  public statusFields: Object = { text: "StatusText", value: "StatusText" };
+  public StatusData: Object[] = [
+     { StatusText: "Entreprise" },
+     { StatusText: "Télétravail" },
+     { StatusText: "Mission" }
+  ];
+
+  public eventobject: EventSettingsModel = {
+    dataSource: [{
+      Id: 1,
+      Subject: "Meditation time",
+      StartTime: new Date(2019,0,17,11,0),
+      EndTime: new Date(2019,8,17,11,38),
+      Location: "yaseer"
+    }]
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public dateParser(data: string) {
+    console.log(data)
+    return new Date(data);
+  }
 }
--- src/app/activite/activite.module.ts
+++ src/app/activite/activite.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ActiviteRoutingModule } from './activite-routing.module';
 import { ActiviteComponent } from './activite.component';
 import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService } from '@syncfusion/ej2-angular-schedule';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
 
 @NgModule({
   declarations: [ActiviteComponent],
   imports: [
     CommonModule,
     ActiviteRoutingModule,
-    ScheduleModule, RecurrenceEditorModule
+    ScheduleModule, RecurrenceEditorModule, DropDownListModule, DateTimePickerModule
   ],
   providers: [DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService],
   bootstrap: [ActiviteComponent]
